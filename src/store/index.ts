798d818a23cc8e92d1a400eff973acf91192fdef
aishacodes <aishatabdulfatah81@gmail.com>
2022-10-20 06:35:10 +0100

    fix: store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,35 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartReducer";
-import storage from "redux-persist/lib/storage";
-
-import {
-  persistReducer,
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-
-const persistConfig = {
-  key: "blogs",
-  storage,
-};
-
-const cartpersistedReducer = persistReducer(persistConfig, cartReducer);
 
 const store = configureStore({
   reducer: {
-    cartReducer: cartpersistedReducer,
+    cartReducer: cartReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
 
 export default store;
